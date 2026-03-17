67249e33f75b20d861888885a489a864aea827a1
dassoham030701 <soham1937@gmail.com>
2026-03-18 00:18:31 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Figtree } from "next/font/google";
+import { Geist, Geist_Mono, Figtree,DM_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
 const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const dmSans = DM_Sans({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  // weight: ["400", "500", "700"],
+  // variable: "--font-dm-sans",
 });
 
 export const metadata: Metadata = {
@@ -28,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={cn("font-sans", figtree.variable)}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={dmSans.className}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <h2>Welcome to FluxUI</h2>
+      <Button>ok</Button>
     </div>
   );
 }
